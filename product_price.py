50e275746f345f1e7e7ede5a7117d5f9d40447fa
NehaSutar27 <nehalaturkar@gmail.com>
2025-12-01 22:14:53 +0000

    python practice

---
+++ product_price.py
@@ -0,0 +1,60 @@
+toyota= 42000
+honda= 38000
+ford= 35000
+bmw= 55000
+print("Price of Toyota : " , toyota)
+print("Price of Honda : " , honda)
+print("Price of Ford : " , ford)
+print("Price of BMW : " , bmw)
+total_price= toyota + honda + ford + bmw
+print("Total Price of all cars : " , total_price)
+if bmw > toyota:
+    print("BMW is more expensive than Toyota")
+else:
+    print("BMW is not more expensive than Toyota")
+print("The most expensive car is : ", bmw)    
+print(5>3)
+print("Hello", "World!", sep="-")
+print(3 * 'abc')
+
+a, b, c = "Appple", "Banana", "Cherry"
+print(a, b, c, sep=", ")
+print(a)
+
+d=f=e = "Same Value"
+print(d, f, e, sep=" | ")
+#this is collection
+fruits = ["Apple", "Banana", "Cherry"]
+n,m,o = fruits
+print(n)
+print(m)
+print(o)
+h="Python "
+i="is"
+j=" fun"
+print(h+i+j)
+# typecadting
+val1 = 10
+val2 = 20.5
+val3 = "30"
+total = val1 + val2 + int(val3)
+print("Total:", total)
+print(str(val3))
+print(float(val2))
+# Variables to store personal information
+name = "Neha Sutar"
+age = 25
+city = "Norwich"
+hobby = "Playing guitar"
+
+# Display the personal information card
+print("==================================")
+print("       Personal Information")
+print("==================================")
+print("Name:  ", name)
+print("Age:   ", age)
+print("City:  ", city)
+print("Hobby: ", hobby)
+print("==================================")
+print(type(age))
+print(type(val2))
--- sample.py
+++ sample.py
@@ -1,2 +1,15 @@
 x=10
 print("The value of x is:", x)
+y=20
+a ="Hello"
+print("The value of y is:", y)
+z=x+y
+print("The sum of x and y is:", z)
+if x>y:
+    print("x is greater than y")
+else:
+    print("x is not greater than y")
+    # from this section
+    # comments lines starts
+    # dont worry 
+print(x,a);    
